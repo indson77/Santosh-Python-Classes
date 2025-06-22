f2ac40acba21c80584287470ccd1a2bdaf9a4cc9
indson77 <indson.77@gmail.com>
2025-06-22 14:52:11 +0530

    first commit

---
+++ program.py
@@ -0,0 +1,90 @@
+# print('How')
+
+# i = 0 
+# while i <= 10 :
+#     print(i)
+#     i += 1
+
+# def comparison():
+#    x = int(input("enter your first value"))
+#    y = int(input("enter your second value"))
+#    if x > y :
+#     print("x is greater than y")
+#    else :
+#     print("x is smaller than y")
+# comparison()
+
+# x = 10
+# y = 20
+# x += y
+# print(x)
+
+# l = [ ]
+# while True :
+#     students_marks = int(input("enter your marks : "))
+#     if students_marks == 100 :
+#         break
+#     l.append(students_marks)
+
+
+# Questions: 4, 15,16,18, 21, 24, 30 
+
+
+# 4 area of the circle 
+radius_of_circle = int(input("enter the radius of circle : "))
+area = 3.14*radius_of_circle*radius_of_circle 
+print(area)
+
+# 16 diffrence from 17 
+num_ = int(input("enter your number : "))
+sub_ = 17 - num_
+if num_ > 17 :
+    print(sub_*2)
+else :
+    print(sub_)
+
+# 15 volume of sphere of radius 6 
+radius = int(input("enter the radius of sphere :"))
+surface_area = 4/3*3.14*radius**3
+if radius == 6 :
+    print(surface_area)
+else :
+    print("write radius value equal to 6 ")
+
+# 18 sum of three number
+a = int(input("entar your first value :"))
+b = int(input("enter your second value :"))
+c = int(input("enter you third value :"))
+sum = a + b + c
+if a == b == c :
+    print(sum*3)
+else :
+    print(sum)
+
+# 21 even or odd checker 
+value_ = int(input("enter your value"))
+if value_%2 == 0 :
+    print("the given no is even number")
+else :
+    print("the given no is odd number")
+
+# 24 vowel tester 
+letters = input("enter the any letter whatever you want :")
+if letters == 'a':
+    print("letter is vowel")
+elif letters == 'e':
+    print("letter is vowel")
+elif letters == 'i':
+    print("letter is vowel")
+elif letters == 'o':
+    print("letter is vowel")
+elif letters == 'u':
+    print("letter is vowel")
+else:
+    print("letter is consonant") 
+
+# 30 area of triangle 
+base = int(input("enter the value of base of triangle :"))
+height = int(input("enter the height of the triangle :"))
+area_of_triangle = 1/2*base*height
+print("area of triangle = ", area_of_triangle)  
